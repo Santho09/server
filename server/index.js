7779bbf229d275b759cf17a0d6b19397b10be475
Santho09 <santhoshpriya0104@gmail.com>
2025-01-22 18:15:39 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
 });
 
 // Server
-const PORT =  8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });

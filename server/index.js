d3d1b0fccd2702ba66d7e8905f391a584b019fc8
Santho09 <santhoshpriya0104@gmail.com>
2025-01-22 19:32:38 +0530

    second change

--- server/index.js
+++ server/index.js
@@ -26,6 +26,12 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+
+app.get('/api/healthcheck', (req, res) => {
+    res.status(200).json({ message: 'API is working' });
+});
+
+
 // CORS setup (Allow requests from localhost:3000)
 app.use(cors({
     origin: 'http://localhost:3000' // Hard-coded to allow requests from your React frontend or change it accordingly

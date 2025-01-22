adf8f42aafe411258e8efb556ff40110c67e650d
Santho09 <santhoshpriya0104@gmail.com>
2025-01-22 22:13:41 +0530

    last commit

--- server/controller/blog.js
+++ server/controller/blog.js
@@ -1,13 +1,15 @@
 import Blog from "../models/blog.js";
-import user from "../models/user.js";
+import User from "../models/user.js";
 
+// Create a new blog
 export const createBlog = async (req, res) => {
     const { title, description, author, selectedFile, tags } = req.body;
     try {
-        const existing_user = await user.findById(author);
-        if (!existing_user) {
+        const existingUser = await User.findById(author);
+        if (!existingUser) {
             return res.status(404).json({ mssg: "User doesn't exist" });
         }
+
         const newBlog = new Blog({
             title,
             description,
@@ -18,70 +20,99 @@ export const createBlog = async (req, res) => {
         await newBlog.save();
         return res.status(201).json({ mssg: "Blog created successfully", blog: newBlog });
     } catch (error) {
-        return res.status(500).json({ mssg: "Something went wrong" });
+        return res.status(500).json({ mssg: "Something went wrong", error: error.message });
     }
 };
 
-export const getAllBlogs=async(req,res)=>{
-    try{
-        const blogs=await Blog.find({})
-        return res.status(200).json({blogs})
+// Get all blogs
+export const getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find({});
+        return res.status(200).json({ blogs });
+    } catch (error) {
+        return res.status(500).json({ mssg: "Something went wrong", error: error.message });
     }
-    catch(error){
-        return res.status(500).json({mssg:"Something went wrong"})
-    }
-}
-export const getBlogById=async(req,res)=>{
-    const {id}=req.params
-    try{
-        const blog=await Blog.findById(id)
-        if(!blog){
-            return res.status(404).json({mssg:'Blog not found'})
-        }
-        return res.status(200).json({blog})
-    }
-    catch(error){
-        console.error(error); 
-        return res.status(500).json({mssg:"Something went wrong",error:error.message})
-    }
-}
-export const getBlogBySearch=async(req,res)=>{
-    const {searchQuery,tags}=req.query
-    console.log(searchQuery,tags)
-    try{
-      const title=new RegExp(searchQuery,'i')
+};
 
-      let blogs;
-      if(tags){
-        blogs=await Blog.find({$or:[{title},{tags:{$in:tags.split(',')}}]})
-      }
-      else{
-        blogs=await Blog.find({title})
-      }
-      return res.status(200).json({blogs})
+// Get blog by ID
+export const getBlogById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const blog = await Blog.findById(id);
+        if (!blog) {
+            return res.status(404).json({ mssg: "Blog not found" });
+        }
+        return res.status(200).json({ blog });
+    } catch (error) {
+        return res.status(500).json({ mssg: "Something went wrong", error: error.message });
     }
-    catch(error){
-        return res.status(404).json({mssg:"Something went wrong"})
+};
+
+// Search blogs by title or tags
+export const getBlogBySearch = async (req, res) => {
+    const { searchQuery, tags } = req.query;
+    try {
+        const title = new RegExp(searchQuery, 'i'); // 'i' for case-insensitive search
+
+        let blogs;
+        if (tags) {
+            blogs = await Blog.find({ $or: [{ title }, { tags: { $in: tags.split(',') } }] });
+        } else {
+            blogs = await Blog.find({ title });
+        }
+        return res.status(200).json({ blogs });
+    } catch (error) {
+        return res.status(404).json({ mssg: "Something went wrong", error: error.message });
     }
-}
-export const updateBlog=async(req,res)=>{
-    const {id}=req.params
-    const {title,description,selectedFile,tags}=req.body
-    try{
-        const updatedBlog=await Blog.findByIdAndUpdate(id,{title,description,selectedFile,tags},{new:true})
-        return res.status(200).json({updatedBlog})
+};
+
+// Update a blog (using POST for update)
+export const updateBlog = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, selectedFile, tags } = req.body;
+
+    try {
+        const blog = await Blog.findById(id);
+        if (!blog) {
+            return res.status(404).json({ mssg: "Blog not found" });
+        }
+
+        // Check if the logged-in user is the author of the blog
+        if (blog.author.toString() !== req.userId) {
+            return res.status(403).json({ mssg: "Unauthorized to update this blog" });
+        }
+
+        // Update the blog with the new values
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            id,
+            { title, description, selectedFile, tags },
+            { new: true } // Return the updated document
+        );
+
+        return res.status(200).json({ mssg: "Blog updated successfully", updatedBlog });
+    } catch (error) {
+        return res.status(500).json({ mssg: "Something went wrong", error: error.message });
     }
-    catch(error){
-        return res.status(500).json({mssg:'Something went wrong'})
+};
+
+// Delete a blog
+export const deleteBlog = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const blog = await Blog.findById(id);
+        if (!blog) {
+            return res.status(404).json({ mssg: "Blog not found" });
+        }
+
+        // Check if the logged-in user is the author of the blog
+        if (blog.author.toString() !== req.userId) {
+            return res.status(403).json({ mssg: "Unauthorized to delete this blog" });
+        }
+
+        await Blog.findByIdAndDelete(id);
+        return res.status(200).json({ mssg: "Blog deleted successfully" });
+    } catch (error) {
+        return res.status(500).json({ mssg: "Something went wrong", error: error.message });
     }
-}
-export const deleteBlog=async(req,res)=>{
-    const {id}=req.params
-    try{
-        await Blog.findByIdAndDelete(id)
-        return res.status(200).json({mssg:"Blog deleted successfully"})
-    }
-    catch(error){
-        return res.status(500).json({mssg:'Something went wrong'})
-    }
-}
+};
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,12 +1,15 @@
-import express from 'express'
-import { createBlog, deleteBlog, getAllBlogs, getBlogById, getBlogBySearch, updateBlog } from '../controller/blog.js'
+import express from 'express';
+import { createBlog, deleteBlog, getAllBlogs, getBlogById, getBlogBySearch, updateBlog } from '../controller/blog.js';
+import auth from '../middleware/index.js'; // Import authentication middleware
 
-const router=express.Router()
+const router = express.Router();
 
-router.post('/',createBlog)
-router.get('/',getAllBlogs)
-router.get('/search',getBlogBySearch)
-router.get('/:id',getBlogById)
-router.post('/:id',updateBlog)
-router.delete('/:id',deleteBlog)
-export default router
+// Routes
+router.post('/', auth, createBlog); // Create blog (protected route)
+router.get('/', getAllBlogs); // Get all blogs (public route)
+router.get('/search', getBlogBySearch); // Search blogs (public route)
+router.get('/:id', getBlogById); // Get a single blog by ID (public route)
+router.post('/:id', auth, updateBlog); // Update blog by ID using POST (protected route)
+router.delete('/:id', auth, deleteBlog); // Delete blog by ID (protected route)
+
+export default router;
